b32ed76bf22b338bdf4258f70c79391865445ce2
souqkplatinum-gif <souqkplatinum@gmail.com>
2026-02-08 10:07:24 +0200

    Restore base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/safwa/',
   plugins: [react()],
+  base: '/safwa/',   // 👈 مهم جدًا
 })
